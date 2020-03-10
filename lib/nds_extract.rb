10fe44469c0c19ebab01199aab9b4f9fca56ca9c
John Tom Jr <johntjr.22@gmail.com>
2020-03-10 01:15:59 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -34,6 +34,14 @@ end
 # Your code after this point
 
 def movies_with_director_key(name, movies_collection)
+  result = []
+  i = 0 
+  
+  while i < movies_collection.length do
+  new_data = movies_collection[i] 
+  result << movie_with_director_name(name, new_data)
+   i += 1
+  
   # GOAL: For each Hash in an Array (movies_collection), provide a collection
   # of movies and a directors name to the movie_with_director_name method
   # and accumulate the returned Array of movies into a new Array that's
@@ -49,9 +57,22 @@ def movies_with_director_key(name, movies_collection)
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
 end
-
+result
+end
 
 def gross_per_studio(collection)
+  result = {} 
+  i = 0 
+  while i < collection.length do 
+    cool = collection[i]
+    
+    if !result[cool[:studio]]
+      result[cool[:studio]] = cool[:worldwide_gross]
+  else
+    result[cool[:studio]] += cool[:worldwide_gross]
+  end
+  i+=1
+  
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
   # each studio.
@@ -64,8 +85,20 @@ def gross_per_studio(collection)
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
 end
+result
+end
 
 def movies_with_directors_set(source)
+  result = []
+  i = 0 
+  
+  while i < source.length do 
+    d_info = source[i]
+    d_name = d_info[:name]
+    d_movies = d_info[:movies]
+    
+    result << movies_with_director_key(d_name,d_movies)
+    i +=1 
   # GOAL: For each director, find their :movies Array and stick it in a new Array
   #
   # INPUT:
@@ -77,7 +110,8 @@ def movies_with_directors_set(source)
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
 end
-
+result
+end
 # ----------------    End of Your Code Region --------------------
 # Don't edit the following code! Make the methods above work with this method
 # call code. You'll have to "see-saw" to get this to work!
